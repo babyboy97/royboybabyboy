13ff2e62dad138fb32135ab39c3b42464411e114
babyboy97 <roy.chap13@gmail.com>
2021-10-03 17:46:46 -0700

    feat: added most of the CSS functionality or at least played with it. Added some to about page and pinned header to bottom.

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-# Defining the home page of our site
-# @app.route("/")  # this sets the route to this page
-# def home():
-# 	return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
-
 @app.route("/")
 @app.route('/home')
 def home():
